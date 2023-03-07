df2e8291d211ba474a3e862f0dfb580ef3f36c9a
Berlin Experiment <info@berlin-experiment.com>
2023-03-07 13:18:01 +0100

    created version enum, implementation & test

--- http/src/httprequest.rs
+++ http/src/httprequest.rs
@@ -1,3 +1,4 @@
+// * method ---------------------------------------------------------- //
 #[derive(Debug, PartialEq)]
 pub enum Method {
     Get,
@@ -15,6 +16,24 @@ impl From<&str> for Method {
     }
 }
 
+// * version ---------------------------------------------------------- //
+#[derive(Debug, PartialEq)]
+pub enum Version {
+    V1_1,
+    V2_0,
+    Uninitialized,
+}
+
+impl From<&str> for Version {
+    fn from(s: &str) -> Version {
+        match s {
+            "HTTP/1.1" => Version::V1_1,
+            _ => Version::Uninitialized,
+        }
+    }
+}
+
+// ? tests ---------------------------------------------------------- //
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -23,5 +42,11 @@ mod tests {
     fn test_method_into() {
         let m: Method = "GET".into();
         assert_eq!(m, Method::Get);
+    }
+
+    #[test]
+    fn test_version_into() {
+        let m: Version = "HTTP/1.1".into();
+        assert_eq!(m, Version::V1_1)
     }
 }
